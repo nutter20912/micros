33bbb29d39a00d73ce1431f343e882761e591ed3
paul <nutter20912@gmail.com>
2023-11-23 16:29:35 +0800

    market task add restart

--- app/market/tasks/get_tickers.go
+++ app/market/tasks/get_tickers.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"micros/app/market/binance"
 	"micros/database/redis"
+	"time"
 
 	"go-micro.dev/v4"
 )
@@ -16,36 +17,46 @@ type getTickers struct {
 
 func (e *getTickers) Run() {
 	rdb := redis.Get()
-	stream, err := binance.NewClient(context.Background()).MiniTickersStream()
-	if err != nil {
-		log.Fatalln(err)
+
+	getTickersFunc := func() {
+		stream, err := binance.NewClient(context.Background()).MiniTickersStream()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		defer stream.Close()
+
+		for {
+			rsp, err := stream.ReadMessage()
+			if err != nil {
+				fmt.Println(err)
+				break
+			}
+
+			tickers, ok := rsp.(*binance.MiniTickerArrMessage)
+			if !ok {
+				continue
+			}
+
+			if len(tickers.Data) == 0 {
+				continue
+			}
+
+			price := map[string]interface{}{}
+			for _, v := range tickers.Data {
+				price[v.Symbol] = v.Close
+			}
+
+			if err := rdb.HSet(context.Background(), "price", price).Err(); err != nil {
+				log.Println(err)
+			}
+		}
 	}
 
-	defer stream.Close()
-
 	for {
-		rsp, err := stream.ReadMessage()
-		if err != nil {
-			fmt.Println(err)
-			break
-		}
+		getTickersFunc()
 
-		tickers, ok := rsp.(*binance.MiniTickerArrMessage)
-		if !ok {
-			continue
-		}
-
-		if len(tickers.Data) == 0 {
-			continue
-		}
-
-		price := map[string]interface{}{}
-		for _, v := range tickers.Data {
-			price[v.Symbol] = v.Close
-		}
-
-		if err := rdb.HSet(context.Background(), "price", price).Err(); err != nil {
-			log.Println(err)
-		}
+		time.Sleep(time.Second)
+		fmt.Println("restart getTickersFunc")
 	}
 }
